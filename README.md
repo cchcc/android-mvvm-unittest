0c2c9a08f411e6e1724a3f8dae21340a0c571504
cchcc <jo.cchcc@gmail.com>
2018-06-06 23:48:27 +0900

    E01 simple activity

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # android-mvvm-unittest
-to learn Android Architecture Components and unit test
+to learn android [MVVM](https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93viewmodel) based on [Android Architecture Components](https://developer.android.com/topic/libraries/architecture/) and unit test
+
+
+## E01 - Simple Activity
+
+- [Data Binding](https://developer.android.com/topic/libraries/data-binding/)
+- [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel)
+- [LiveData](https://developer.android.com/topic/libraries/architecture/livedata)
--- app/src/main/java/cchcc/learn/amu/MainActivity.kt
+++ app/src/main/java/cchcc/learn/amu/MainActivity.kt
@@ -1,12 +1,23 @@
 package cchcc.learn.amu
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cchcc.learn.amu.databinding.ActivityMainBinding
+import cchcc.learn.amu.e01.E01Activity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        with(DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)) {
+            host = this@MainActivity
+        }
     }
+
+    fun startE01() {
+        startActivity(Intent(this, E01Activity::class.java))
+    }
+
 }
